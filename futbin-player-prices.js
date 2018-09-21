8504b44c01d9880dc82965265ec653c95cb3b13f
Luis Azevedo <labraceta@gmail.com>
2018-09-22 00:33:31 +0100

    Add support for Xbox, PC and PS4 platforms

--- futbin-player-prices.js
+++ futbin-player-prices.js
@@ -19,8 +19,8 @@
 // ==/UserScript==
 
 var main = function() {
-    "use strict";
-    $("head").append(`<style id="addedCSS" type="text/css">
+  "use strict";
+  $("head").append(`<style id="addedCSS" type="text/css">
   #TradePile .player-stats-data-component, #Unassigned .player-stats-data-component {
     width: 12em;
   }
@@ -107,170 +107,180 @@ var main = function() {
 
   </style>`);
 
-    $("body").append(
-        '<div class="popup" style="display:none"><span "info">Best Page Deal:<span> <span class="name"></span> <span class="price"></span> <span class="gain"></span></div>'
-    );
-    var globalInterval;
+  $("body").append(
+    '<div class="popup" style="display:none"><span "info">Best Page Deal:<span> <span class="name"></span> <span class="price"></span> <span class="gain"></span></div>'
+  );
+  var globalInterval;
 
-    function showFutbinPriceInPage(listRows, futbinData) {
-        console.log("showFutbinPriceInPage", listRows, futbinData);
-        var targetDomElements = jQuery(".listFUTItem").get();
+  function getPersonaPlatform() {
+    var platform = "";
+    if (services.User.getUser().getSelectedPersona().isPlaystation) platform = "ps";
+    if (services.User.getUser().getSelectedPersona().isPC) platform = "pc";
+    if (services.User.getUser().getSelectedPersona().isXbox) platform = "xbox";
 
-        var bestDeal = {
-            item: "",
-            price: 0,
-            futbinPrice: 0,
-            gain: 0
-        };
+    return platform;
+  
+}
+  function showFutbinPriceInPage(listRows, futbinData) {
+    console.log("showFutbinPriceInPage", listRows, futbinData);
+    var targetDomElements = jQuery(".listFUTItem").get();
 
-        listRows.map((item, idx) => {
-            if (!futbinData) {
-                return;
-            }
-            var target = jQuery(targetDomElements[idx]);
-            if (!item.data.isPlayer()) {
-                return;
-            }
+    var bestDeal = {
+      item: "",
+      price: 0,
+      futbinPrice: 0,
+      gain: 0
+    };
 
-            var playerId = item.data.resourceId;
+    listRows.map((item, idx) => {
+      if (!futbinData) {
+        return;
+      }
+      var target = jQuery(targetDomElements[idx]);
+      if (!item.data.isPlayer()) {
+        return;
+      }
 
-            var platform = "ps";
-            if (!futbinData[playerId]) {
-                return; // futbin data might not be available for this player
-            }
+      var playerId = item.data.resourceId;
 
-            if (target.find(".futbin").length > 0) {
-                return; // futbin price already added to the row
-            }
+      var platform = getPersonaPlatform();
 
-            var targetForButton = null;
-            var futbinPriceRaw = futbinData[playerId].prices[platform].LCPrice;
+      if (!futbinData[playerId]) {
+        return; // futbin data might not be available for this player
+      }
 
-            if (getCurrentControllerClassName() === "UTMarketSearchResultsSplitViewController") {
-                // Stuff for transfermarket
-                var futbinPrice = parseInt(futbinPriceRaw.replace(/,/g, ""));
+      if (target.find(".futbin").length > 0) {
+        return; // futbin price already added to the row
+      }
 
-                var isBuyNowLowerThanFutbinPrice = item.data.getAuctionData().buyNowPrice < futbinPrice;
-                var delta = item.data.getAuctionData().buyNowPrice - futbinPrice;
+      var targetForButton = null;
+      var futbinPriceRaw = futbinData[playerId].prices[platform].LCPrice;
 
-                var okToBuyClass = isBuyNowLowerThanFutbinPrice ? "oktobuy" : "";
-                var deltaClass = delta < 0 ? "delta-green" : "delta-red";
-                var deltaString = delta > 0 ? "+" + delta : delta;
-                futbinPriceRaw = futbinPriceRaw + '<span class="' + deltaClass + '">(' + deltaString + ")</span>";
-                console.log("Comparing: ", delta, bestDeal.gain, bestDeal);
-                if (delta < bestDeal.gain) {
-                    bestDeal.gain = delta;
-                    bestDeal.name = item.data.getStaticData().name;
-                    bestDeal.futbinPrice = futbinPrice;
-                    bestDeal.price = item.data.getAuctionData().buyNowPrice;
-                }
-            }
+      if (getCurrentControllerClassName() === "UTMarketSearchResultsSplitViewController") {
+        // Stuff for transfermarket
+        var futbinPrice = parseInt(futbinPriceRaw.replace(/,/g, ""));
 
-            // jQuery(".secondary.player-stats-data-component").css("float", "left");
-            targetForButton = target.find(".auction");
-            targetForButton.show();
-            targetForButton.prepend(
-                '<div class="auctionValue futbin ' +
-                    okToBuyClass +
-                    '"><span class="label">Futbin <span class="futbinupdate">(' +
-                    futbinData[playerId].prices[platform].updated +
-                    ')</span></span><span class="coins value">' +
-                    futbinPriceRaw +
-                    "</span></div>"
-            );
-        });
+        var isBuyNowLowerThanFutbinPrice = item.data.getAuctionData().buyNowPrice < futbinPrice;
+        var delta = item.data.getAuctionData().buyNowPrice - futbinPrice;
 
-        if (getCurrentControllerClassName() === "UTMarketSearchResultsSplitViewController") {
-            $(".popup").show();
-            if (bestDeal.gain === 0) {
-                // reset
-                $(".popup .name").text("Nothing found");
-                $(".popup .price").text("");
-                $(".popup .gain").text("");
-            } else {
-                $(".popup .name").text(bestDeal.name);
-                $(".popup .price").text(bestDeal.price);
-                $(".popup .gain").text(bestDeal.gain + " (coins cheaper)");
-            }
+        var okToBuyClass = isBuyNowLowerThanFutbinPrice ? "oktobuy" : "";
+        var deltaClass = delta < 0 ? "delta-green" : "delta-red";
+        var deltaString = delta > 0 ? "+" + delta : delta;
+        futbinPriceRaw = futbinPriceRaw + '<span class="' + deltaClass + '">(' + deltaString + ")</span>";
+        console.log("Comparing: ", delta, bestDeal.gain, bestDeal);
+        if (delta < bestDeal.gain) {
+          bestDeal.gain = delta;
+          bestDeal.name = item.data.getStaticData().name;
+          bestDeal.futbinPrice = futbinPrice;
+          bestDeal.price = item.data.getAuctionData().buyNowPrice;
         }
+      }
+
+      // jQuery(".secondary.player-stats-data-component").css("float", "left");
+      targetForButton = target.find(".auction");
+      targetForButton.show();
+      targetForButton.prepend(
+        '<div class="auctionValue futbin ' +
+          okToBuyClass +
+          '"><span class="label">Futbin <span class="futbinupdate">(' +
+          futbinData[playerId].prices[platform].updated +
+          ')</span></span><span class="coins value">' +
+          futbinPriceRaw +
+          "</span></div>"
+      );
+    });
+
+    if (getCurrentControllerClassName() === "UTMarketSearchResultsSplitViewController") {
+      $(".popup").show();
+      if (bestDeal.gain === 0) {
+        // reset
+        $(".popup .name").text("Nothing found");
+        $(".popup .price").text("");
+        $(".popup .gain").text("");
+      } else {
+        $(".popup .name").text(bestDeal.name);
+        $(".popup .price").text(bestDeal.price);
+        $(".popup .gain").text(bestDeal.gain + " (coins cheaper)");
+      }
     }
+  }
 
-    function getFutbinPlayerPrices(listRows) {
-        var playersIds = listRows.map(a => (a.data.isPlayer() ? a.data.resourceId : null)).filter(a => a !== null);
-        var futbinUrl = "https://www.futbin.com/19/playerPrices?player=&all_versions=" + playersIds.join(",");
+  function getFutbinPlayerPrices(listRows) {
+    var playersIds = listRows.map(a => (a.data.isPlayer() ? a.data.resourceId : null)).filter(a => a !== null);
+    var futbinUrl = "https://www.futbin.com/19/playerPrices?player=&all_versions=" + playersIds.join(",");
 
-        new GM_xmlhttpRequest({
-            method: "GET",
-            url: futbinUrl,
-            onload: function(res) {
-                var futbinData = JSON.parse(res.response);
-                console.log("Found futbin prices for current page");
-                showFutbinPriceInPage(listRows, futbinData);
+    new GM_xmlhttpRequest({
+      method: "GET",
+      url: futbinUrl,
+      onload: function(res) {
+        var futbinData = JSON.parse(res.response);
+        console.log("Found futbin prices for current page");
+        showFutbinPriceInPage(listRows, futbinData);
 
-                // clearInterval(globalInterval);
-            }
-        });
+        // clearInterval(globalInterval);
+      }
+    });
+  }
+
+  function getCurrentControllerClassName() {
+    return getAppMain()
+      .getRootViewController()
+      .getPresentedViewController()
+      .getCurrentViewController()
+      .getCurrentController().className;
+  }
+
+  function getPagePlayerData() {
+    if (document.querySelector(".futbin")) {
+      return; // futbin price already added to the page
     }
+    $(".popup").hide();
+    var playerElements = document.querySelectorAll(".listFUTItem .player");
+    if (playerElements.length > 0) {
+      // players found
+      var currentViewControllerClassName = getCurrentControllerClassName();
 
-    function getCurrentControllerClassName() {
+      // Club search or Player search
+      if (
+        currentViewControllerClassName === "ClubSearchResultsSplitViewController" ||
+        currentViewControllerClassName === "UTMarketSearchResultsSplitViewController"
+      ) {
         return getAppMain()
-            .getRootViewController()
-            .getPresentedViewController()
-            .getCurrentViewController()
-            .getCurrentController().className;
+          .getRootViewController()
+          .getPresentedViewController()
+          .getCurrentViewController()
+          .getCurrentController()._listController._view._list._listRows;
+      }
+
+      // Transfer list
+      if (
+        currentViewControllerClassName === "UTTransferListSplitViewController" ||
+        currentViewControllerClassName === "UTWatchListSplitViewController"
+      ) {
+        return getAppMain()
+          .getRootViewController()
+          .getPresentedViewController()
+          .getCurrentViewController()
+          .getCurrentController()
+          ._listController._view._sections.flatMap(a => a._listRows);
+      }
+
+      return;
+    } else {
+      return;
     }
+  }
 
-    function getPagePlayerData() {
-        if (document.querySelector(".futbin")) {
-            return; // futbin price already added to the page
-        }
-        $(".popup").hide();
-        var playerElements = document.querySelectorAll(".listFUTItem .player");
-        if (playerElements.length > 0) {
-            // players found
-            var currentViewControllerClassName = getCurrentControllerClassName();
+  function run() {
+    globalInterval = setInterval(() => {
+      var listRows = getPagePlayerData();
 
-            // Club search or Player search
-            if (
-                currentViewControllerClassName === "ClubSearchResultsSplitViewController" ||
-                currentViewControllerClassName === "UTMarketSearchResultsSplitViewController"
-            ) {
-                return getAppMain()
-                    .getRootViewController()
-                    .getPresentedViewController()
-                    .getCurrentViewController()
-                    .getCurrentController()._listController._view._list._listRows;
-            }
-
-            // Transfer list
-            if (
-                currentViewControllerClassName === "UTTransferListSplitViewController" ||
-                currentViewControllerClassName === "UTWatchListSplitViewController"
-            ) {
-                return getAppMain()
-                    .getRootViewController()
-                    .getPresentedViewController()
-                    .getCurrentViewController()
-                    .getCurrentController()
-                    ._listController._view._sections.flatMap(a => a._listRows);
-            }
-
-            return;
-        } else {
-            return;
-        }
-    }
-
-    function run() {
-        globalInterval = setInterval(() => {
-            var listRows = getPagePlayerData();
-
-            if (listRows) {
-                getFutbinPlayerPrices(listRows);
-            }
-        }, 1000);
-    }
-    run();
+      if (listRows) {
+        getFutbinPlayerPrices(listRows);
+      }
+    }, 1000);
+  }
+  run();
 };
 
 main();
